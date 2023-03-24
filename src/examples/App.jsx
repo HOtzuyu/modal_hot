6f738f1f04c29c0ac636314ba2ec43a90f9434d9
Jonhathan HO TZU YU <hotzuyu@hotzuyu-graphic.fr>
2023-03-24 16:49:37 +0100

    init modal

--- src/examples/App.jsx
+++ src/examples/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import Modale from "../lib";
 
 export const App = () => {
+  const [displayModal, setDisplayModal] = useState(false);
   return (
-    <Modale
-      title="Titre"
-      text="Le texte"
-      cross="X"
-      button="close"
-    />
+    <>
+      <button onClick={() => setDisplayModal(true)}>Show Modal</button>
+      <Modale
+        title="Titre"
+        text="Le texte"
+        cross="X"
+        button="close"
+        showModal={displayModal}
+        hideModal={() => setDisplayModal(false)}
+      />
+    </>
   );
 };
--- src/lib/Modal.jsx
+++ src/lib/Modal.jsx
@@ -1,19 +1,36 @@
 import React from "react";
 import "./Modal.css";
 
-export const Modale = ({ title, cross, text, button }) => {
+export const Modale = ({
+  title,
+  cross,
+  text,
+  button,
+  showModal,
+  hideModal,
+}) => {
   return (
-    <>
-      <div className="modal">
-        <div className="modal__header">
-          <h5 className="modal__header--title">{title}</h5>
-          <button className="modal__header--button modal__button">
-            {cross}
+    showModal && (
+      <>
+        <div className="modal">
+          <div className="modal__header">
+            <h5 className="modal__header--title">{title}</h5>
+            <button
+              className="modal__header--button modal__button"
+              onClick={hideModal}
+            >
+              {cross}
+            </button>
+          </div>
+          <p className="modal__text">{text}</p>
+          <button
+            className="modal__button modal__close"
+            onClick={hideModal}
+          >
+            {button}
           </button>
         </div>
-        <p className="modal__text">{text}</p>
-        <button className="modal__button modal__close">{button}</button>
-      </div>
-    </>
+      </>
+    )
   );
 };

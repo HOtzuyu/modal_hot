9602c5f053a945c54fb3774ea773e4875073c33a
Jonhathan HO TZU YU <hotzuyu@hotzuyu-graphic.fr>
2023-03-24 17:05:25 +0100

    cleanning json

---
+++ dist/Modal.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var _interopRequireDefault = require("D:/openclassroom/modal_lib/modal_lib-ho/node_modules/@babel/runtime/helpers/interopRequireDefault.js").default;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Modale = void 0;
+var _react = _interopRequireDefault(require("react"));
+require("./Modal.css");
+var Modale = function Modale(_ref) {
+  var title = _ref.title,
+    cross = _ref.cross,
+    text = _ref.text,
+    button = _ref.button,
+    showModal = _ref.showModal,
+    hideModal = _ref.hideModal;
+  return showModal && /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal"
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal__header"
+  }, /*#__PURE__*/_react.default.createElement("h5", {
+    className: "modal__header--title"
+  }, title), /*#__PURE__*/_react.default.createElement("button", {
+    className: "modal__header--button modal__button",
+    onClick: hideModal
+  }, cross)), /*#__PURE__*/_react.default.createElement("p", {
+    className: "modal__text"
+  }, text), /*#__PURE__*/_react.default.createElement("button", {
+    className: "modal__button modal__close",
+    onClick: hideModal
+  }, button)));
+};
+exports.Modale = Modale;
---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _Modal = require("./Modal");
+var _default = _Modal.Modale;
+exports.default = _default;
